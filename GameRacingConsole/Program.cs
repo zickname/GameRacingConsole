cff89a2f2ca8bc1d998e4e341fea7a0e0c784c50
Aydar U. <aidohha@gmail.com>
2024-02-22 12:57:21 +0600

    update

--- GameRacingConsole/Program.cs
+++ GameRacingConsole/Program.cs
@@ -6,8 +6,15 @@ Console.Write($"Чтобы начать игру, нажмите 'Enter': ");
 
 while (true)
 {
-    if (Console.ReadKey(true).Key != ConsoleKey.Enter) continue;
-    game.RunRace();
-    Console.ReadKey();
-    break;
+    if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+    {
+        game.RunRace();
+        Console.ReadKey();
+        break;
+    }
+
+    if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+    {
+        break;
+    }
 }
